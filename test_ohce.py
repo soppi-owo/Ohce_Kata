08cdac07b75d585f69034b58e5a5d652fe46decf
Soppi <sofibravo123.sb@gmail.com>
2025-04-09 22:50:56 -0400

    added night time greetint test (non passing)

--- test_ohce.py
+++ test_ohce.py
@@ -12,3 +12,9 @@ def test_greeting_afternoon(mocker):
     mocked_datetime.now.return_value = datetime(2025, 4, 9, 15, 0)  # 15:00
     ohce = Ohce("Pedro")
     assert ohce.greet() == "¡Buenas tardes Pedro!"
+
+def test_greeting_night(mocker):
+    mocked_datetime = mocker.patch("ohce.datetime")
+    mocked_datetime.now.return_value = datetime(2025, 4, 9, 22, 0)  # 22:00
+    ohce = Ohce("Pedro")
+    assert ohce.greet() == "¡Buenas noches Pedro!"
