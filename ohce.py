45c1d561e6ff7fd1dd25963daf256ddc5aff2445
Soppi <sofibravo123.sb@gmail.com>
2025-04-09 22:51:48 -0400

    added night time greeting code (passing)

--- ohce.py
+++ ohce.py
@@ -10,3 +10,5 @@ class Ohce:
             return f"¡Buenos días {self.name}!"
         elif 12 <= hour < 20:
             return f"¡Buenas tardes {self.name}!"
+        else:  # 20-6
+            return f"¡Buenas noches {self.name}!"
